26fb774fdb3812168acd85cbdbcf73c755cf1e43
Caelinh <Caelinhargrave@gmail.com>
2022-06-21 16:48:07 -0500

    completed some styling as well as got a chart displayed. Also no repeats on pictures.

--- code.js
+++ code.js
@@ -2,6 +2,11 @@
 
 let chartEl = document.getElementById('myChart');
 let ctx = chartEl.getContext('2d');
+var options = { 
+    responsive: true,
+    maintainAspectRatio: true
+}
+
 let pageElements = document.querySelectorAll('img');//selects all image tags
 let resultsButton = document.querySelector('#button');
 let results = document.querySelector('#Info');
@@ -59,14 +64,7 @@ let image1 = generateRandomImage();
 let image2 = generateRandomImage();
 let image3 = generateRandomImage();
 
-// assigning objects in code to page elements to display on
-pageElements[0].id = image1.id;
-pageElements[0].src = image1.src;
-pageElements[1].id = image2.id;
-pageElements[1].src = image2.src;
-pageElements[2].id = image3.id;
-pageElements[2].src = image3.src;
-
+renderImages();
 //checking that whats being clicked is in the image sources and adding a click
 function handleClick(event) {
     for (let i = 0; i < images.length; i++) {
@@ -88,39 +86,35 @@ function generateRandomImage() {
     return images[index];
 }
 
-//checking the random images for duplicates and rendering to page by assigning to page elements
 
-function renderImages() {
+function noDuplicates(){
     let currentImage1 = pageElements[0].id
     let currentImage2 = pageElements[1].id
     let currentImage3 = pageElements[2].id
+
+    while (image1.id === currentImage1 || image1.id === currentImage2 || image1.id === currentImage3) {
+        image1 = generateRandomImage();
+    }
+
+    while (image2.id === currentImage1 || image2.id === currentImage2 || image2.id === currentImage3) {
+        image2 = generateRandomImage();
+    }
+
+    while (image3.id === currentImage1 || image3.id === currentImage2 || image3.id === currentImage3) {
+        image3 = generateRandomImage();
+    }
+
+}
+//checking the random images for duplicates and rendering to page by assigning to page elements
+
+function renderImages() {
+    noDuplicates();
+  while (image1.id === image2.id || image2.id === image3.id || image1.id === image3.id){
     image1 = generateRandomImage();
     image2 = generateRandomImage();
     image3 = generateRandomImage();
-        while (image1.id === currentImage1 || image1.id === currentImage2 || image1.id === currentImage3) {
-            
-            while (image1.id === image2.id || image1.id === image3.id){
-                image1 = generateRandomImage();
-            }
-            image1 = generateRandomImage();
-        }
-
-        while (image2.id === currentImage1 || image2.id === currentImage2 || image2.id === currentImage3) {
-            
-            while (image1.id === image2.id || image2.id === image3.id){
-                image2 = generateRandomImage();
-            }
-            image2 = generateRandomImage();
-        }
-
-        while (image3.id === currentImage1 || image3.id === currentImage2 || image3.id === currentImage3) {
-            
-            while (image1.id === image3.id || image2.id === image3.id){
-                image3 = generateRandomImage();
-            }
-            image3 = generateRandomImage();
-        }
-
+    noDuplicates();
+  }
 
     pageElements[0].id = image1.id;
     pageElements[0].src = image1.src;
@@ -134,11 +128,12 @@ function renderImages() {
     roundCounter++;
     if (roundCounter == 25) {
         images.forEach(function (img) {
-            let dataSlot = document.getElementById('Info')
-            let name = document.createElement('p')
-            dataSlot.appendChild(name)
+            // let dataSlot = document.getElementById('Info')
+            // let name = document.createElement('p')
+            // dataSlot.appendChild(name)
 
-            name.textContent = 'Image: ' + img.id + '  Clicks: ' + img.clicks + ' views: ' + img.views;
+            // name.textContent = 'Image: ' + img.id + '  Clicks: ' + img.clicks + ' views: ' + img.views;
+            resultsButton.classList.toggle('hide');
             resultsButton.classList.toggle('reveal');
 
         })
